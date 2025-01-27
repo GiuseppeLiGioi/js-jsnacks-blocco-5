30bfb1e1706f66c99035f6c03a106aab437c2189
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-01-27 15:26:50 +0100

    ADD: snack 2

--- snack-2/script.js
+++ snack-2/script.js
@@ -3,6 +3,16 @@ const people = [
   { name: 'Giulia', age: 24 },
   { name: 'Marco', age: 67 }
 ];
+// con for
+for(let i = 0; i < people.length; i++){
+  console.log(people[i].name);
+}
+
+//con foreach
+people.forEach(element => {
+  
+  return console.log(element.name)
+});
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
