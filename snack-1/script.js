dc0218f55a4c1c0d3bc9d3a11d29b7c8366e20b7
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-01-27 15:21:51 +0100

    CREATE: workspace, add snack1

---
+++ snack-1/script.js
@@ -0,0 +1,17 @@
+const names = ['Edoardo', 'Simone', 'Francesco'];
+
+
+
+console.log(names);
+
+
+
+//con foreach
+names.forEach(element => {
+
+    return console.log(element)
+    
+});
+
+// Stampa in console tutti i nomi
+// Risultato: 'Edoardo', 'Simone', 'Francesco'
